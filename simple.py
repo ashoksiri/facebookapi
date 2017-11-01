ff970bde685c7a6289292bffb2a09f7cf2efe627
ashoksiri <ashoksiri@yahoo.com>
2017-11-01 23:15:43 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -4,4 +4,7 @@ access_token='1257032691047126|mP2cUAf8cIH-ZA0WntiqScJT8HI'
 version = '2.10'
 graph = Graph(access_token,version)
 
-print graph.getpages('narendramodi',limit=100)
+pages = graph.getPages('narendramodi',limit=5)
+
+for page in pages:
+    print page.id,graph.getPage(id=page.id)
